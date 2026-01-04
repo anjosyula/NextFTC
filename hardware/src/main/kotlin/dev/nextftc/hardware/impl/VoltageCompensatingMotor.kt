8f04201f4fc765a635c7479fae3caf11a1c3e3c5
aneesh <aneeshjosyula@gmail.com>
2026-01-04 01:23:13 -0500

    added voltage comp

--- hardware/src/main/kotlin/dev/nextftc/hardware/impl/VoltageCompensatingMotor.kt
+++ hardware/src/main/kotlin/dev/nextftc/hardware/impl/VoltageCompensatingMotor.kt
@@ -1,50 +1,16 @@
 package dev.nextftc.hardware.impl
 
-import dev.nextftc.core.units.parseDuration
-import dev.nextftc.ftc.ActiveOpMode
 import dev.nextftc.hardware.controllable.Controllable
 import kotlin.properties.Delegates
-import kotlin.time.ComparableTimeMark
-import kotlin.time.Duration
-import kotlin.time.Duration.Companion.seconds
-import kotlin.time.TimeSource.Monotonic.markNow
 
 class VoltageCompensatingMotor(
     private val controllable: Controllable,
-    private val voltageCacheTime: Duration,
+    private val voltageFunction: () -> Double,
     private val nominalVoltage: Double = 12.0
 ) : Controllable by controllable {
 
-    @JvmOverloads
-    constructor(
-        controllable: Controllable,
-        voltageCacheTimeSeconds: Double = 0.5,
-        nominalVoltage: Double = 12.0,
-    ) : this(controllable, voltageCacheTimeSeconds.seconds, nominalVoltage)
-
-    @JvmOverloads
-    constructor(
-        controllable: Controllable,
-        voltageCacheTime: String,
-        nominalVoltage: Double = 12.0
-    ) : this(controllable, parseDuration(voltageCacheTime), nominalVoltage)
-
-    private val voltageSensor by lazy { ActiveOpMode.hardwareMap.voltageSensor.first() }
-
-    private var cachedVoltage by Delegates.notNull<Double>()
-    private lateinit var lastVoltageRead: ComparableTimeMark
-
-    private val voltage: Double
-        get() {
-            val currentTime = markNow()
-            if (!::lastVoltageRead.isInitialized || currentTime - lastVoltageRead >= voltageCacheTime) {
-                cachedVoltage = voltageSensor.voltage
-                lastVoltageRead = currentTime
-            }
-            return cachedVoltage
-        }
 
     override var power by Delegates.observable(0.0) { _, _, new ->
-        controllable.power = new * nominalVoltage / voltage
+        controllable.power = new * nominalVoltage / voltageFunction.invoke()
     }
 }
